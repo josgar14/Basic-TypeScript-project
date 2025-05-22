11ffca333e4e3751501638801da5e10a95cd6ee1
Josue <josgar_14@hotmail.com>
2025-05-22 16:05:15 -0400

    first commit

---
+++ src/ts/models/Pizza.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Pizza = void 0;
+var DataResource_1 = require("../services/DataResource");
+exports.Pizza = new DataResource_1.DataResource('http://localhost:3000/pizzas');
---
+++ src/ts/models/Pizza.ts
@@ -0,0 +1,13 @@
+import { DataResource } from "../services/DataResource";
+
+export interface PizzaProps {
+    title: string,
+    description: string,
+    toppings: string[],
+    price: number
+}
+
+export const Pizza = new DataResource<PizzaProps>(
+    'http://localhost:3000/pizzas'
+)
+
